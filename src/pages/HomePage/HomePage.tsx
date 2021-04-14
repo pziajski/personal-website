0dfa971b27590236b5706f8482d0aeb44b440678
Patrick <patrick.ziajski@gmail.com>
2021-04-14 16:07:35 -0400

    simple section examples including star fall animation

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { StarFall } from '../../components/StarFall/StarFall';
+import "./HomePage.scss";
 
 export const HomePage = () => {
     return (
         <div className="home-page">
-            
+            <div id="about" className="section-container">
+                <StarFall />
+                <h2 className="section-title">Software Developer with an interest in softare, hardware, space, and physics.</h2>
+            </div>
+            <div id="github" className="section-container">
+                <h1>Github</h1>
+            </div>
         </div>
     )
 }
