fb8acbfa6b6cb30c49684a69b389cc19a2666d03
Patrick <patrick.ziajski@gmail.com>
2021-04-14 16:06:52 -0400

    star fall component

---
+++ src/components/StarFall/StarFall.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./StarFall.scss";
+
+export const StarFall = () => {
+    return (
+        <div className="starfall-animation">
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+        </div>
+    )
+}
