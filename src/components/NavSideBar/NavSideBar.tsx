5da764cd17f1f8f1ed401a3ae24a781bb6fef6f2
Patrick <patrick.ziajski@gmail.com>
2021-04-14 16:33:02 -0400

    created a container incase more a footer is added

--- src/components/NavSideBar/NavSideBar.tsx
+++ src/components/NavSideBar/NavSideBar.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { FilePerson, Github } from "react-bootstrap-icons";
+import { FilePerson, Github, Linkedin } from "react-bootstrap-icons";
 import "./NavSideBar.scss";
 
 export const NavSideBar = () => {
     return (
         <nav className="nav-bar">
-            <a href="#about">
-                <FilePerson className="nav-bar__icon" />
-            </a>
-            <a href="#github">
-                <Github className="nav-bar__icon" />
-            </a>
+            <div className="nav-bar__sections">
+                <a href="#about">
+                    <FilePerson className="nav-bar__icon" />
+                </a>
+                <a href="#github">
+                    <Github className="nav-bar__icon" />
+                </a>
+                <a href="#linkedin">
+                    <Linkedin className="nav-bar__icon" />
+                </a>
+            </div>
         </nav>
     )
 }
