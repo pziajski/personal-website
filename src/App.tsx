9ccdc44b2a5aad4d795402a1861c365bed127411
Patrick <patrick.ziajski@gmail.com>
2021-04-15 01:23:22 -0400

    removed page indicators

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.scss';
 import { MainContent } from './components/MainContent/MainContent';
-import { PageIndicator } from './components/PageIndicator/PageIndicator';
 
 function App() {
     return (
         <div className="App">
             <MainContent />
-            <PageIndicator />
         </div>
     );
 }
