fbc19b0ae1e03f23d33194a5ef81ec70be94b18b
Patrick <patrick.ziajski@gmail.com>
2021-04-18 01:12:16 -0400

    changed classnames, included temp title

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,21 +7,25 @@ export const Home = () => {
         <div className="main-content">
             <div className="main-content__landing">
                 <StarFall />
-                <h2 className="main-content__landing-title">Software Developer with an interest in software, hardware, space, and physics.</h2>
+                <h2 className="main-content__title">Software Developer with an interest in software, hardware, space, and physics.</h2>
+                <h3 className="main-content__sub-title">Full-Stack focus</h3>
             </div>
             <div className="main-content__info">
-                <ul className="main-content__info-list">
-                    <li>
-                        <a href="https://github.com/pziajski">
-                            <Github className="icon" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/patrick-ziajski/">
-                            <Linkedin className="icon" />
-                        </a>
-                    </li>
-                </ul>
+                <div className="main-content__info-container">
+                    <h2 className="main-content__info-title">{"[ Project Showcase coming soon ... ]"}</h2>
+                    <ul className="main-content__info-list">
+                        <li>
+                            <a href="https://github.com/pziajski">
+                                <Github className="icon" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/patrick-ziajski/">
+                                <Linkedin className="icon" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <p>{"[ made by patrick for patrick ]"}</p>
             </div>
         </div>
